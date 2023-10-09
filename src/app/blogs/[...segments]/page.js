e4c7541e5c1d4de734a9aae82283276fe0a0eb92
tirahman105 <tirahman105@gmail.com>
2023-10-09 14:02:59 +0600

    useRouter, useParams and useSearchParams hooks

--- src/app/blogs/[...segments]/page.js
+++ src/app/blogs/[...segments]/page.js
@@ -1,10 +1,21 @@
+'use client'
+
+import { useParams, useSearchParams } from 'next/navigation';
 import React from 'react';
 
-const SingleBlog = ({params}) => {
-const [year, id] = (params.segments) || [];
+const SingleBlog = ({params, searchParams}) => {
+const [year, id] = params.segments || [];
+
+const params2 = useParams();
+const searchParams2 = useSearchParams();
+
+console.log(params2.segments, searchParams2.get("title"))
+
     return (
         <div>
             Single Blog {year || new Date().getFullYear()} for {id}
+            <br />
+            {searchParams.title}
         </div>
     );
 };
--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -1,42 +1,67 @@
+// "use client"
 import Link from "next/link";
 
-export const metadata = {
-    title: "AnonnoTa | Blogs",
-    description: "Spread your business with AnonnoTa",
-  };
-  
+import { useRouter } from "next/navigation";
 
-  const blogs = [
-    {
-        id: 1,
-        year: 2023,
-        title: 'Title -1'
-    },
-    {
-        id: 2,
-        year: 2023,
-        title: 'Title -2'
-    },
-    {
-        id: 3,
-        year: 2023,
-        title: 'Title -3'
-    },
-  ]
+
+export const metadata = {
+  title: "AnonnoTa | Blogs",
+  description: "Spread your business with AnonnoTa",
+};
+
+const blogs = [
+  {
+    id: 1,
+    year: 2023,
+    title: "Title -1",
+  },
+  {
+    id: 2,
+    year: 2023,
+    title: "Title -2",
+  },
+  {
+    id: 3,
+    year: 2023,
+    title: "Title -3",
+  },
+];
 
 const BlogsPage = () => {
-    return (
-        <div className="container mx-auto">
-            <h1>Blogs Page</h1>
-            <div className="grid grid-cols-3 gap-6 my-10 ">
+    // const router = useRouter();
+  return (
+    <div className="container mx-auto">
+      <h1>Blogs Page</h1>
+      <div className="grid grid-cols-3 gap-6 my-10 ">
+        {blogs.map(({ id, year, title }) => (
+          <Link
+            className="p-10 bg-teal-400 py-4 border rounded-md shadow-md"
+            href={{
+                pathname: `/blogs/${year}/${id}`,
+                query: {
+                    title: title
+                }
+            }}
+            
+            key={id}
+          >
+            {title}
+          </Link>
 
-            {
-                blogs.map (({id, year, title}) => <Link className="bg-blue-100 p-10 rounded-md shadow-md" href={`/blogs/${year}/${id}`} key={id}>{title}</Link>)
-            }
-            </div>
+          /* <button
+            className="btn bg-teal-400 py-4 border rounded-md shadow-md"
+             onClick={()=> router.push(`/blogs/${year}/${id}?title=${title}`)}
+            key={id}
+          >
+            {title}
+          </button> */
 
-        </div>
-    );
+            
+
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default BlogsPage;
