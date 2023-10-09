efe359faac39ad412ae1ea7844e744d57383b8c6
tirahman105 <tirahman105@gmail.com>
2023-10-09 19:33:46 +0600

    Next Fonts and use with tailwindCSS

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,10 @@
 import "./globals.css";
-import { Inter } from "next/font/google";
+import { Roboto } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto({ subsets: ["latin"], weight: ["100", "300", "400", "500","700", "900" ] ,
+variable: '--font-roboto'
+});
+
 
 export const metadata = {
   title: "AnonnoTa",
@@ -11,7 +14,7 @@ export const metadata = {
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${roboto.variable} font-roboto`}>
        
         {children}
       
